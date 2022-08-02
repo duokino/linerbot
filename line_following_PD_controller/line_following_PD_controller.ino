cbc6903e6ab2c8fcee61f2d64e2a5a47197c2a60
duokino <75552448+duokino@users.noreply.github.com>
2022-08-02 23:26:40 +0800

    Update line_following_PD_controller.ino

--- line_following_PD_controller/line_following_PD_controller.ino
+++ line_following_PD_controller/line_following_PD_controller.ino
@@ -1,168 +1,116 @@
-/*
- This is for Line Following Robot using PD Controller
- for LinerBot
- using Maker Line from cytron
- and TB6612FNG for motor driver
- by Duokino
- */
-
-//Initialization
-int sensor[5] = {0, 0, 0, 0, 0};     //initialization for Maker Line
-int initial_motor_speed = 60;        //can change the initial speed that you prefer
-int maxSpeed = 120;                 //if the motor is too fast, decrease the max speed and vice versa
-float Kp = 6;       //will be change using potentiometer
-float Kd = 12;       //will be change using potettiometer
-int slip = 4;     //will be change using potentiometer
-float VR = A0;    //pin Analog 0 for the input of the potentiometer
-float P=0, D=0, PD_value=0;
-float error=0, previous_error=0;
+//sensor
+#define VR A0     //Potentiometer
+int S1 = 0;
+int S2 = 0;
+int S3 = 0;
+int S4 = 0;
+int S5 = 0;
 
 //Pin declaration for TB6612FNG
 
 //Motor Right
-int PWMB = 10; //Speed control
-int BIN1 = 12; //Direction
-int BIN2 = 11; //Direction
+#define PWMB 10 //Speed control
+#define BIN1 12 //Direction
+#define BIN2 11 //Direction
 
 //Motor Left
-int PWMA = 3; //Speed control
-int AIN1 = 5; //Direction
-int AIN2 = 4; //Direction
+#define PWMA 3 //Speed control
+#define AIN1 5 //Direction
+#define AIN2 4 //Direction
 
-void read_sensor_values(void);
-void calculate_pid(void);
-void motor_control(void);
+int motorSpeed;
+int error;
+int position;
+
+    int BaseSpeed = 120;
+    float Kp = 0.06; 
+    float Kd = 4; 
+    float Ki = 0.000; 
+    int integral = 0;
+    int lastError = 0; 
+    int MotorA_Speed = 0; 
+    int MotorB_Speed = 0; 
+    int offset = 2000;
+
 
 void setup() {
-
-  Serial.begin(9600);       //Enable Serial Communications
-  pinMode (VR, INPUT);   //Pin Setup for potentiometer
   
-  //Maker Line Auto-Calibrating Line Sensor Pin Setup
-  pinMode(A1,INPUT);
-  pinMode(A2,INPUT);
-  pinMode(A3,INPUT);
-  pinMode(A4,INPUT);
-  pinMode(A5,INPUT);
+  pinMode (VR, INPUT);
+  pinMode (A1, INPUT);
+  pinMode (A2, INPUT);
+  pinMode (A3, INPUT);
+  pinMode (A4, INPUT);
+  pinMode (A5, INPUT);
 
   //Motor Driver Pin Setup
-  pinMode(PWMB,OUTPUT);
-  pinMode(BIN1,OUTPUT);
-  pinMode(BIN2,OUTPUT);
-  pinMode(PWMA,OUTPUT);
-  pinMode(AIN1,OUTPUT);
-  pinMode(AIN2,OUTPUT);
-  digitalWrite(BIN1,LOW);    //according to your connection from motor to the motor driver
-  digitalWrite(BIN2,HIGH);      //according to your connection from motor to the motor driver
-  digitalWrite(AIN1,LOW);      //according to your connection from motor to the motor driver
-  digitalWrite(AIN2,HIGH);      //according to your connection from motor to the motor driver
-  
-  melody();
-  
+  pinMode (PWMB,OUTPUT);
+  pinMode (BIN1,OUTPUT);
+  pinMode (BIN2,OUTPUT);
+  pinMode (PWMA,OUTPUT);
+  pinMode (AIN1,OUTPUT);
+  pinMode (AIN2,OUTPUT);
+
+  Serial.begin (9600);
+
 }
 
 void loop() {
 
-    read_sensor_values();
-    calculate_pid();
-    motor_control();
-
-}
-
-void read_sensor_values()
-{
-  sensor[0] = digitalRead(A1);
-  sensor[1] = digitalRead(A2);
-  sensor[2] = digitalRead(A3);
-  sensor[3] = digitalRead(A4);
-  sensor[4] = digitalRead(A5); 
+  //read sensor
+  S1 = digitalRead(A1);
+  S2 = digitalRead(A2);
+  S3 = digitalRead(A3);
+  S4 = digitalRead(A4);
+  S5 = digitalRead(A5);
   
-  if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==1) && (sensor[3]==0) && (sensor[4]==0))          //center
-  error = 0;
-  else if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==1) && (sensor[3]==1) && (sensor[4]==0))     //out left
-  error = 1;
-  else if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==0) && (sensor[3]==1) && (sensor[4]==0))     //out left
-  error = 2;
-  else if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==0) && (sensor[3]==1) && (sensor[4]==1))     //out left
-  error = 4;
-  else if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==0) && (sensor[3]==0) && (sensor[4]==1))     //out left
-  error = 3;
-  else if((sensor[0]==0) && (sensor[1]==1) && (sensor[2]==1) && (sensor[3]==0) && (sensor[4]==0))     //out right
-  error = -1;
-  else if((sensor[0]==0) && (sensor[1]==1) && (sensor[2]==0) && (sensor[3]==0) && (sensor[4]==0))     //out right
-  error = -2;
-  else if((sensor[0]==1) && (sensor[1]==1) && (sensor[2]==0) && (sensor[3]==0) && (sensor[4]==0))     //out right
-  error = -4;
-  else if((sensor[0]==1) && (sensor[1]==0) && (sensor[2]==0) && (sensor[3]==0) && (sensor[4]==0))     //out right
-  error = -3;
-  else if((sensor[0]==0) && (sensor[1]==0) && (sensor[2]==0) && (sensor[3]==0) && (sensor[4]==0))     //out of range
-    if(error==5) error = 5;
-    else error = -5;
-}
+  if (S1 == 1 && S2 == 0 && S3 == 0 && S4 == 0 && S5 == 0) position = 3500;
+  else if (S1 == 0 && S2 == 1 && S3 == 0 && S4 == 0 && S5 == 0) position = 2500;
+  else if (S1 == 0 && S2 == 0 && S3 == 1 && S4 == 0 && S5 == 0) position = 2000;
+  else if (S1 == 0 && S2 == 0 && S3 == 0 && S4 == 1 && S5 == 0) position = 1500;
+  else if (S1 == 0 && S2 == 0 && S3 == 0 && S4 == 0 && S5 == 1) position = 500;
 
-void calculate_pid()
-{  
-    P = error;
-    D = error - previous_error;
-    
-    //Kp = analogRead(VR);
-    //Kp = Kp/100;
-    //Serial.println(Kp);
-    
-    //Kd = analogRead(VR);
-    //Kd = Kd/100;
-    //Serial.println(Kd);
-    
-    PD_value = (Kp*P) + (Kd*D);
-    
-    previous_error = error;
-}
-
-void motor_control()
-{
-    // Calculating the effective motor speed:
-    int left_motor_speed = initial_motor_speed + PD_value;
-    int right_motor_speed = initial_motor_speed - PD_value;
-    
-    // The motor speed should not exceed the max PWM value
-    constrain(left_motor_speed,0,maxSpeed);
-    constrain(right_motor_speed,0,maxSpeed);
-
-    //slip = analogRead(VR);
-    //slip = slip/100;
-    //Serial.println(slip);
-    
-    right_motor_speed = right_motor_speed + slip ; //this line is needed if your motor didn't have same speed
-    
-    //open the Serial Monitor to see the speed of each motor
-    Serial.print ("right = ");
-    Serial.print(right_motor_speed); 
-    Serial.print("\t");
-    Serial.print("left = "); 
-    Serial.println(left_motor_speed);  
-    
-    analogWrite(PWMA,left_motor_speed);   //Left Motor Speed
-    analogWrite(PWMB,right_motor_speed);  //Right Motor Speed
-}
-
-void melody(){
-
-  #define NOTE_B5    988
-  #define NOTE_G5    784
-  #define NOTE_A5    880
-  #define NOTE_DS6  1245
-  #define NOTE_CS6  1109
-  
-  #define BUZZER 8
-  
-  int melody[] = {NOTE_B5, 0, NOTE_G5, NOTE_A5, NOTE_B5, 0, NOTE_G5, NOTE_A5, NOTE_B5, 0, NOTE_B5, NOTE_B5, NOTE_DS6, NOTE_DS6, NOTE_B5, NOTE_B5, NOTE_CS6};
-  int noteDurations[] = {4, 16, 8, 4, 4, 16, 8, 4, 4, 8, 4, 8, 4, 8, 4, 8, 2};
-  
-    for (int thisNote = 0; thisNote < 17; thisNote++){
-      int noteDuration = 1000 / noteDurations[thisNote];
-      tone(BUZZER, melody[thisNote], noteDuration);
-      int pauseBetweenNotes = noteDuration * 1.30;
-      delay(pauseBetweenNotes);
-      noTone(BUZZER);
+  if (position <= 1500){
+    if (S1 == 0 && S2 == 0 && S3 == 0 && S4 == 0 && S5 == 0) position = 0;
     }
+
+  else if (position >= 2500){
+    if (S1 == 0 && S2 == 0 && S3 == 0 && S4 == 0 && S5 == 0) position = 4000;
+    }
+
+  //variable to be adjust
+  //Kp = analogRead(VR)/700;
+  //Serial.print("Kp = ");
+  //Serial.println(Kp);
+
+  //Kd = analogRead(VR)/700;
+  //Serial.print("Kd = ");
+  //Serial.println(Kd);
+
+  //PID calculation  
+
+  error = position - offset;
+      
+  motorSpeed = Kp * error + Kd * (error - lastError) + Ki * integral; 
+  lastError = error;
+
+  //Motor output
+
+  MotorA_Speed = BaseSpeed - motorSpeed ; 
+  MotorB_Speed = BaseSpeed + motorSpeed ; 
+  MotorA_Speed = constrain(MotorA_Speed, 0, 255); 
+  MotorB_Speed = constrain(MotorB_Speed, 0, 255);
+
+  digitalWrite(AIN2, HIGH); 
+  digitalWrite(AIN1, LOW); 
+  analogWrite(PWMA, MotorA_Speed);
+       
+  digitalWrite(BIN2, HIGH); 
+  digitalWrite(BIN1, LOW); 
+  analogWrite(PWMB, MotorB_Speed);
+
 }
+
+//**********best value**********\\
+// Kp=0.06 Kd=4 Speed=80 Time=7.0s
+// Kp=0.2 Kd=5 Speed=120 Time=5.6s
+// Kp=0.06 Kd=4 Speed=120 Time=5.0s
