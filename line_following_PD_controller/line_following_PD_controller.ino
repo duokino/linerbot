e538ba2406cce2329c0c7e1a49b49c346c824a8d
duokino <75552448+duokino@users.noreply.github.com>
2022-08-03 07:50:27 +0800

    Update line_following_PD_controller.ino

--- line_following_PD_controller/line_following_PD_controller.ino
+++ line_following_PD_controller/line_following_PD_controller.ino
@@ -1,3 +1,10 @@
+/*
+ This is for Line Follower
+ using PID Controller for LinerBot
+ Maker Line for cytron for line sensor
+ by Duokino
+ */
+
 //sensor
 #define VR A0     //Potentiometer
 int S1 = 0;
@@ -51,6 +58,8 @@ void setup() {
   pinMode (AIN2,OUTPUT);
 
   Serial.begin (9600);
+
+  melody();
 
 }
 
@@ -108,6 +117,28 @@ void loop() {
   digitalWrite(BIN1, LOW); 
   analogWrite(PWMB, MotorB_Speed);
 
+}
+
+void melody(){
+
+  #define NOTE_B5   988
+  #define NOTE_G5   784
+  #define NOTE_A5   880
+  #define NOTE_DS6  1245
+  #define NOTE_CS6   1109
+  
+  #define BUZZER 8
+  
+  int melody[] = {NOTE_B5, 0, NOTE_G5, NOTE_A5, NOTE_B5, 0, NOTE_G5, NOTE_A5, NOTE_B5, 0, NOTE_B5, NOTE_B5, NOTE_DS6, NOTE_DS6, NOTE_B5, NOTE_B5, NOTE_CS6};
+  int noteDurations[] = {4, 16, 8, 4, 4, 16, 8, 4, 4, 8, 4, 8, 4, 8, 4, 8, 2};
+  
+    for (int thisNote = 0; thisNote < 17; thisNote++){
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(BUZZER, melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(BUZZER);
+    }
 }
 
 //**********best value**********\\
